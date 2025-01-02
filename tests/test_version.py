f79a97139c6a4cff3b3481b84c3390b3ea9e069b
Darian Huotari <darhuotari1@gmail.com>
2025-01-02 11:31:00 -0600

    [fix] - update dependency versioning strategy

--- tests/test_version.py
+++ tests/test_version.py
@@ -4,5 +4,5 @@ from ..src.endpoints import version
 
 def test_list_version():
     """ Test that the module returns the expected version number"""
-    version_number = "0.0.7"
+    version_number = "0.0.8"
     assert version.list_version() == version_number
