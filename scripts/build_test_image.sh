54afb9389f56f2ead28872b16a4aa7a0552b0dcb
Darian <66524965+darianhuotari@users.noreply.github.com>
2025-01-06 11:13:16 -0600

    Update build_test_image.sh

--- scripts/build_test_image.sh
+++ scripts/build_test_image.sh
@@ -5,6 +5,8 @@ sudo apt-get -y install curl
 docker build . -f .Dockerfile -t hivebox:local-ci
 docker run -d -p 8000:8000 hivebox:local-ci
 
+sleep 10
+
 ## curl -s -o /dev/null -w "%{http_code}" localhost:8000/version || { echo '/version endpoint call failed; exiting...' ; exit 1; }
 
 STATUSCODE=$(curl -silent -output /dev/null -w "%{http_code}" http://localhost:8000/version)
